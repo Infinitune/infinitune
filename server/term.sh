615110ff198fd07121bce1f61af55926761d52b0
Andrés Harisa Duvvuri <128623334+vebrasmusic@users.noreply.github.com>
2023-07-16 14:09:15 -0700

    Update term.sh

--- server/term.sh
+++ server/term.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-gnome-terminal
-WID=$(xdotool getactivewindow)
-xdotool type --window $WID 'echo Hello, World!'
-xdotool key --window $WID Return
-
+gnome-terminal # start terminal
+sleep 1 # wait a bit for the terminal to start
+WID=$(xdotool getactivewindow) # get the window ID of the new terminal
+xdotool type --window $WID 'sclang gen.scd' # type the command
+xdotool key --window $WID Return # press Enter
