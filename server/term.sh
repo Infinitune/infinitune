f52ea8b73fde3b0e3b5a7b91e7b031beb81a1efe
Andrés Harisa Duvvuri <128623334+vebrasmusic@users.noreply.github.com>
2023-07-16 13:42:29 -0700

    Update term.sh

--- server/term.sh
+++ server/term.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gnome-terminal -- bash -c 'sclang ~/infinitune/generated_code/gen.scd; sleep 3-; exit' &
+gnome-terminal -- bash -c 'sclang ~/infinitune/generated_code/gen.scd; sleep 40; exit' &
