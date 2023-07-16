d6e005b2a9890e08083e598e1e036c6882f4481e
Andrés Harisa Duvvuri <128623334+vebrasmusic@users.noreply.github.com>
2023-07-16 14:16:47 -0700

    Update term.sh

--- server/term.sh
+++ server/term.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export DISPLAY=:0 # 0
 gnome-terminal & # start terminal
 sleep 10 # wait a bit for the terminal to start
 WID=$(xdotool getactivewindow) # get the window ID of the new terminal
