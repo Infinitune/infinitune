579fdfe180ccfb5627b0cf1ea24d56d2bd6809f9
Andrés Harisa Duvvuri <128623334+vebrasmusic@users.noreply.github.com>
2023-07-16 14:13:14 -0700

    Update term.sh

--- server/term.sh
+++ server/term.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-gnome-terminal # start terminal
-sleep 1 # wait a bit for the terminal to start
+gnome-terminal & # start terminal
+sleep 10 # wait a bit for the terminal to start
 WID=$(xdotool getactivewindow) # get the window ID of the new terminal
 xdotool type --window $WID 'sclang gen.scd' # type the command
 xdotool key --window $WID Return # press Enter
