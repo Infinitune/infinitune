dfaa3fbaf0c8a19354e55bb48a77b18583fddbde
bapic <itsbapic@gmail.com>
2023-07-16 13:45:06 -0700

    More frontend improvements

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ function App() {
     return (
         <DrumMachine
             samples={[
-                { url: "/hat-closed.wav", name: "HIHAT", index: 0 },
+                { url: "/kick.wav", name: "KICK", index: 0 },
                 { url: "/clap.wav", name: "CLAP", index: 1 },
-                { url: "/snare.wav", name: "SD", index: 2 },
-                { url: "/kick.wav", name: "BD", index: 3 },
+                { url: "/hat-closed.wav", name: "HIHAT", index: 2 },
+                { url: "/snare.wav", name: "SD", index: 3 },
                 { url: "/kick.wav", name: "BLAH", index: 4 },
             ]}
         />
