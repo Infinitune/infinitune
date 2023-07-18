c1dd1bd361d4cb0a0b2097b77e700e555df7a5e8
Andrés Harisa Duvvuri <128623334+vebrasmusic@users.noreply.github.com>
2023-07-18 14:20:44 -0700

    Update sound.mjs
    more logging, checking why the api key isn'tloading

--- server/routes/sound.mjs
+++ server/routes/sound.mjs
@@ -7,7 +7,8 @@ import { Configuration, OpenAIApi } from "openai";
 import dotenv from 'dotenv';
 import path from 'path';
 
-dotenv.config({ path: '../.env.local'});
+dotenv.config({ path: '../.env.local', overwrite: true});
+console.log(fs.readFileSync('../.env.local', 'utf8'));
 console.log(process.env);
 
 const key = process.env.OPENAI_API_KEY;
