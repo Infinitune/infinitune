b41237b14e763b0ba39f78563d4220096e9e41ae
Andrés Harisa Duvvuri <128623334+vebrasmusic@users.noreply.github.com>
2023-07-18 15:46:43 -0700

    Update sound.mjs
    changing directory again

--- server/routes/sound.mjs
+++ server/routes/sound.mjs
@@ -101,7 +101,7 @@ temperature: 0
 //const gptOutput = correctSyntax(gptOutputArray.data.choices[0].message.content); (not needed yet)
 const gptOutput = gptOutputArray.data.choices[0].message.content;
 
-const dir = 'generated_code/';
+const dir = '/server/generated_code/';
 
   // Create a .scd file with the output from the GPT model
   const scdFilePath = path.join(dir, `${timestamp}.js`);
